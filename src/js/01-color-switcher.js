017967005bd7786da524c0c1fcbf445cc888345f
Paweł Firlej <polf@polf.pl>
2024-06-19 00:24:27 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+function getWhiteColor(){
+    return document.body.style.backgroundColor = "white";
+    setTimeout(getWhiteColor, 0)
+}    
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]')
+let isChangingColor = false;
+
+function changeBackgroundColor () {
+    document.body.style.backgroundColor = getRandomHexColor(); 
+    setTimeout(changeBackgroundColor, 1000);
+}
+stopButton.disabled = true;
+startButton.addEventListener('click', () => {
+
+    isChangingColor = true;
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    changeBackgroundColor();
+});
+const intervalId = getWhiteColor()
+stopButton.addEventListener('click', () => {
+    isChangingColor = false;
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    clearInterval(intervalId);
+    return getWhiteColor()
+});
+clearInterval(intervalId);
