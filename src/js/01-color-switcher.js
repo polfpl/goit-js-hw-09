e0ac19d489ac69651034d7fd44a559449aac7beb
Paweł Firlej <polf@polf.pl>
2024-06-19 16:36:49 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,19 +2,17 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-function getWhiteColor(){
-    return document.body.style.backgroundColor = "white";
-    setTimeout(getWhiteColor, 0)
-}    
-
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]')
 let isChangingColor = false;
+let interval;
 
 function changeBackgroundColor () {
-    document.body.style.backgroundColor = getRandomHexColor(); 
-    setTimeout(changeBackgroundColor, 1000);
+    interval = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
 }
+
 stopButton.disabled = true;
 startButton.addEventListener('click', () => {
 
@@ -23,12 +21,10 @@ startButton.addEventListener('click', () => {
     stopButton.disabled = false;
     changeBackgroundColor();
 });
-const intervalId = getWhiteColor()
+
 stopButton.addEventListener('click', () => {
     isChangingColor = false;
     startButton.disabled = false;
     stopButton.disabled = true;
-    clearInterval(intervalId);
-    return getWhiteColor()
+    clearInterval(interval);
 });
-clearInterval(intervalId);
